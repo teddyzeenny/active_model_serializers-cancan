09af5fcccd21e5c0f7d5ec1be6f67602373f7e12
Teddy Zeenny <teddyzeenny@gmail.com>
2013-10-02 18:33:04 +0300

    Add support for embedded ids

--- lib/active_model_serializers/cancan/associations.rb
+++ lib/active_model_serializers/cancan/associations.rb
@@ -3,10 +3,17 @@ module ActiveModel
     class Association #:nodoc:
 
       def authorize?
-        !!options[:authorize]
+        options[:authorize] != false
       end
 
       class HasMany #:nodoc:
+        def serialize_ids_with_cancan
+          return serialize_ids_without_cancan unless authorize?
+          object.select {|item| find_serializable(item).can?(:read, item) }.map do |item|
+            find_serializable(item).serializable_hash
+          end
+        end
+        alias_method_chain :serialize_ids, :cancan
 
         def serialize_with_cancan
           return serialize_without_cancan unless authorize?
@@ -19,6 +26,19 @@ module ActiveModel
       end
 
       class HasOne #:nodoc:
+        def serializables_with_cancan
+          serializer = find_serializable(object)
+          return [] unless !authorize? || serializer && serializer.can?(:read, object)
+          serializables_without_cancan
+        end
+        alias_method_chain :serializables, :cancan
+
+        def serialize_ids_with_cancan
+          serializer = find_serializable(object)
+          return nil unless !authorize? || serializer && serializer.can?(:read, object)
+          serialize_ids_without_cancan
+        end
+        alias_method_chain :serialize_ids, :cancan
 
         def serialize_with_cancan
           serializer = find_serializable(object)
